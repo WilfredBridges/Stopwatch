2dc5b4e9a8f76de21f47a623feb908d28ed8e95f
Wilfred <wbridges905@gmail.com>
2023-06-09 13:56:15 +0100

    Stopwatch app initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  let timerInterval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        milliseconds += 1;
+        if (milliseconds === 100) {
+            milliseconds = 0;
+            seconds++;
+        }
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        if (seconds < 10) {
+          document.getElementById("timer").innerHTML = `${minutes}:0${seconds}:${milliseconds};`;
+        } else {
+          document.getElementById("timer").innerHTML = `${minutes}:${seconds}:${milliseconds};`;
+        }
+    }, 10);
+
+} 
+
+function stopTimer() {
+    clearInterval(timerInterval);
+
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    document.getElementById("timer").innerHTML = "00:00:00";
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Wilfred I Love You
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+    <h3>Timer: <span id="timer">00:00:00</span></h3>
+    <div class="center">
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+    </div>
     </div>
   );
 }
